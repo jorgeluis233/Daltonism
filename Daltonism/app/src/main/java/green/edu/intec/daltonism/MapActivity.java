f976c961e1dd33f01a813ed33f486ad4ed4208b3
jorgeluis233 <jorgesantana_23@hotmail.com>
2015-04-19 23:54:43 -0400

    Final results are displayed
    Results with daltonism level are displayed with percentage

--- Daltonism/app/src/main/java/green/edu/intec/daltonism/MapActivity.java
+++ Daltonism/app/src/main/java/green/edu/intec/daltonism/MapActivity.java
@@ -1,6 +1,8 @@
 package green.edu.intec.daltonism;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +10,23 @@ import android.view.MenuItem;
 
 import android.location.Location;
 import android.location.LocationManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MapActivity extends ActionBarActivity {
@@ -23,6 +34,9 @@ public class MapActivity extends ActionBarActivity {
     private MapFragment mapView;
     private GoogleMap map;
     private LatLng currLatLng;
+    private LocationManager locationManager;
+    private String provider;
+    private ArrayList<Question> questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,8 @@ public class MapActivity extends ActionBarActivity {
         setContentView(R.layout.activity_map);
 
         Intent intent = getIntent();
+        questions = (ArrayList<Question>)intent.getSerializableExtra("QUESTION_LIST");
+        showResults();
 
         //Setting up the map
         // Gets the MapView from the XML layout and creates it
@@ -54,6 +70,7 @@ public class MapActivity extends ActionBarActivity {
                 MapActivity.this.updateMarker(latLng);
             }
         });
+
 
     }
 
@@ -81,13 +98,46 @@ public class MapActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void showResults(){
+
+        TextView textResult = (TextView)findViewById(R.id.viewTextResult);
+        TextView textAverage = (TextView)findViewById(R.id.textViewAverage);
+        String level = "";
+        double count = 0;
+
+        for (Question q : questions){
+            if(q.isCorrect()){
+                count++;
+            }
+        }
+
+        double percentage = (count/38)*100;
+        DecimalFormat df = new DecimalFormat("0.00");
+        int t = (int)count;
+        textAverage.setText("You got "+ t+ " out of 38 answers correct for a "+df.format(percentage)+"% accuracy");
+        if(count >=32){
+            level = "Zero";
+        } else if(count >= 27){
+            level = "Fair";
+        }
+        else if(count >= 22){
+            level = "Moderate";
+        } else {
+            level = "Severe";
+        }
+
+        textResult.setText(level);
+
+    }
+
     private void updateMarker(LatLng latLong){
 
         currLatLng = latLong;
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currLatLng, 16);
 
         map.clear();
-        map.addMarker(new MarkerOptions().position(currLatLng).title("this"));
+        map.addMarker(new MarkerOptions().position(currLatLng).title("Nearest"));
         map.animateCamera(cameraUpdate);
     }
+
 }
